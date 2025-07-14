7eb319cd9397eb6b3907289e138c751a2b2821d2
jimlee <jim19911126@gmail.com>
2025-07-14 16:32:34 +0800

    修正版型 把fieldset修正只有刪除的區塊

--- back/acc.php
+++ back/acc.php
@@ -21,54 +21,51 @@
 
                 </tr>
                 <?php
-                endforeach;
-                ?>
+            endforeach;
+            ?>
         </table>
-        <div>
+        <div class="ct">
             <input type="submit" value="確定刪除">
             <input type="reset" value="清除">
         </div>
     </form>
-
-
-    <h1>新增會員</h1>
-    <form>
-        <div class="ct" style="color: red;">請設定您要註冊的帳號及密碼(最長12個字元)</div>
-        <table>
-            <tr>
-                <td>Step1:登入帳號</td>
-                <td>
-                    <input type="text" name="acc" id="acc">
-                </td>
-            </tr>
-            <tr>
-                <td>Step2:登入密碼</td>
-                <td>
-                    <input type="password" name="pw" id="pw">
-                </td>
-            </tr>
-            <tr>
-                <td>Step3:再次確認密碼</td>
-                <td>
-                    <input type="password" name="pw2" id="pw2">
-                </td>
-            </tr>
-            <tr>
-                <td>Step4:信箱(忘記密碼時使用)</td>
-                <td>
-                    <input type="text" name="email" id="email">
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <input type="button" value="註冊" onclick="reg()">
-                    <input type="button" value="清除">
-                </td>
-                <td></td>
-            </tr>
-        </table>
-    </form>
 </fieldset>
+
+<h1>新增會員</h1>
+<div class="ct" style="color: red;">請設定您要註冊的帳號及密碼(最長12個字元)</div>
+<table>
+    <tr>
+        <td>Step1:登入帳號</td>
+        <td>
+            <input type="text" name="acc" id="acc">
+        </td>
+    </tr>
+    <tr>
+        <td>Step2:登入密碼</td>
+        <td>
+            <input type="password" name="pw" id="pw">
+        </td>
+    </tr>
+    <tr>
+        <td>Step3:再次確認密碼</td>
+        <td>
+            <input type="password" name="pw2" id="pw2">
+        </td>
+    </tr>
+    <tr>
+        <td>Step4:信箱(忘記密碼時使用)</td>
+        <td>
+            <input type="text" name="email" id="email">
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <input type="button" value="註冊" onclick="reg()">
+            <input type="button" value="清除">
+        </td>
+        <td></td>
+    </tr>
+</table>
 
 <script>
     function reg(params) {
