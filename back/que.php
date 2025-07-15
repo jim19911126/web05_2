124b1f86077c99753eb3805cc90a536cbe480e39
jimlee <jim19911126@gmail.com>
2025-07-15 10:16:41 +0800

    修改最新消息 但目前無法正常使用顯示 刪除 更新等功能 尚未除錯成功

---
+++ back/que.php
@@ -0,0 +1,12 @@
+<fieldset>
+    <legend>問卷調查</legend>
+    <form action="./api/admin_que.php" method="post">
+        <div style="display: flex;">
+            <div style="width: 50%;">問卷名稱</div>
+            <div style="width: 50%;">
+                <input type="text" name="subject" id="subject">
+            </div>
+        </div>
+        
+    </form>
+</fieldset>
--- front/login.php
+++ front/login.php
@@ -9,7 +9,7 @@
             </tr>
             <tr>
                 <td>密碼</td>
-                <td><input type="text" name="pw" id="pw"></td>
+                <td><input type="password" name="pw" id="pw"></td>
             </tr>
             <tr>
                 <td>
