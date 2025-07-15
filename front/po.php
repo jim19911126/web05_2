8a84851dfbf70ec0b8b34f03b4fc91664ede3dde
jimlee <jim19911126@gmail.com>
2025-07-15 13:45:14 +0800

    點選文章列表顯示整篇文章的功能完成

--- front/po.php
+++ front/po.php
@@ -1,6 +1,6 @@
 <div class="nav" style="margin-bottom: 20px;">
 
-目前位置：首頁 > 分類網誌 > <span id='NavType'>慢性病防治</span>
+    目前位置：首頁 > 分類網誌 > <span id='NavType'>慢性病防治</span>
 </div>
 <fieldset style="width:120px; display:inline-block; vertical-align:top">
     <legend>分類網誌</legend>
@@ -9,7 +9,7 @@
     <div><a class="type-link" data-type="3">癌症防治</a></div>
     <div><a class="type-link" data-type="4">慢性病防治</a></div>
 </fieldset>
-<fieldset style="width: 600px; display: inline-block;">
+<fieldset style="width: 580px; display: inline-block;">
     <legend>文章列表</legend>
     <div id="TypeList"></div>
     <div id="Post"></div>
@@ -17,15 +17,43 @@
 </fieldset>
 
 <Script>
-    $(".type-link").on("click",function name(params) {
-        let type=$(this).text();
+    getList(1);
+
+
+    $(".type-link").on("click", function name(params) {
+        let type = $(this).text();
         $("#NavType").text(type);
-        let typeId=$(this).data("type");
-        $.get("api/get_type_list.php", {type:typeId},function name(list) {
+        let typeId = $(this).data("type");
+        getList(typeId);
+        // $.get("api/get_type_list.php", {type:typeId},function name(list) {
+        //     $("#Post").html("");
+        //     $("#TypeList").html(list);
+
+        // })
+
+    })
+
+    function getPost(id) {
+        $.get("./api/get_post.php", { id }, function name(post) {
+            $("#TypeList").html("");
+            $("#Post").html(post);
+        })
+    }
+    $(".post-item").on("click", function name(params) {
+        let postId = $(this).data("post");
+        getPost(postId);
+
+    })
+
+    function getList(type) {
+        $.get("api/get_type_list.php", { type }, function name(list) {
             $("#Post").html("");
             $("#TypeList").html(list);
-            
+            $(".post-item").on("click", function name(params) {
+                let postId = $(this).data("post");
+                getPost(postId);
+
+            })
         })
-        
-    })
+}
 </Script>
